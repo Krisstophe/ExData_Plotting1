d2adc88f5c6fe28486fc233fa64540895fb01040
Krisstophe <go.bi@live.cn>
2014-07-14 12:17:50 +0800

    Course Project 1

---
+++ Rscript/plot4.R
@@ -0,0 +1,13 @@
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", stringsAsFactors = FALSE)
+subdata <- data[grep("^[1-2]/2/2007", data$Date),]
+subdata$Date_Time <- strptime(paste(subdata$Date, subdata$Time), "%d/%m/%Y %H:%M:%S")
+par(mfrow = c(2,2))
+plot(subdata$Date_Time, subdata$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(subdata$Date_Time, subdata$Voltage, type = "l", xlab = "datetime", ylab = "Voltage")
+plot(subdata$Date_Time, subdata$Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering")
+lines(subdata$Date_Time, subdata$Sub_metering_2, type = "l", col = "red")
+lines(subdata$Date_Time, subdata$Sub_metering_3, type = "l", col = "blue")
+legend("topright", inset = -0.02, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1,1), col = c("black", "red", "blue"), cex = 0.5, bty = "n", y.intersp = 0.7)
+plot(subdata$Date_Time, subdata$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power", lwd = 0.1)
+dev.copy(png, file = "plot4.png")
+dev.off()
